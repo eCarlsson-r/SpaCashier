df93761f58f2628ede2df926a30b02387e4f19a0
E. Carlsson R. <e.carlsson.r@gmail.com>
2026-04-14 20:53:29 +0700

    Fix production information for remote image loading

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,33 @@
 import type { NextConfig } from "next";
 
-const url = (process.env.NEXT_PUBLIC_API_URL ?? "").split(":");
-const currentHost = (url.length > 2) ? {
-  protocol: url[0],
-  hostname: url[1].replace("//", ""),
-  port: url[2],
-  pathname: '/storage/**', // A glob pattern for the path
-} : {
-  protocol: url[0],
-  hostname: url[1].replace("//", ""),
-  pathname: '/storage/**', // A glob pattern for the path
-};
-
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [currentHost],
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost', // The hostname of your external images
+        port: '8000',
+        pathname: '/storage/**', // A glob pattern for the path
+      }
+    ],
   },
 };
+
+const url = (process.env.NEXT_PUBLIC_API_URL ?? "").split(":");
+if (url.length > 1) {
+    const currentHost = (url.length > 2) ? {
+    protocol: url[0],
+    hostname: url[1].replace("//", ""),
+    port: url[2],
+    pathname: '/storage/**', // A glob pattern for the path
+  } : {
+    protocol: url[0],
+    hostname: url[1].replace("//", ""),
+    pathname: '/storage/**', // A glob pattern for the path
+  };
+
+  nextConfig.images?.remotePatterns?.push(currentHost);
+}
 
 export default nextConfig;
